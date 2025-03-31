4a06f612dc383f383c68c947d541e8bca05f6b55
jcm <galahad000310@gmail.com>
2025-03-31 18:10:23 +0900

    [level 2] Title: 튜플, Time: 63.83 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,45 +1,28 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
-        
+        s = s.substring(2, s.length() - 2).replace("},{", "-");
+        String[] arr = s.split("-");
+
         List<List<Integer>> list = new ArrayList<>();
-        int idx = 0;
-        StringBuilder num = new StringBuilder();
-        list.add(new ArrayList<>());
-        for(int i = 1; i<s.length()-1; i++){
-            char c = s.charAt(i);
-            if(c == '{'){
-                list.add(new ArrayList<>());
-                idx++;
-            }else if(Character.isDigit(c)){
-                num.append(c);
-            }else{
-                if(!num.toString().equals("")){
-                    list.get(idx).add(Integer.parseInt(num.toString()));
-                    num = new StringBuilder();
-                }
+        for(String str : arr){
+            List<Integer> temp = new ArrayList<>();
+            for(String num : str.split(",")){
+                temp.add(Integer.parseInt(num));
+            }
+            list.add(temp);
+        }
+
+        list.sort(Comparator.comparingInt(List::size));
+
+        Set<Integer> set = new LinkedHashSet<>();
+        for(List<Integer> l : list){
+            for(int num : l){
+                set.add(num);
             }
         }
-        
-        Collections.sort(list, (o1, o2) -> o1.size() - o2.size());
-        
-        Set<Integer> answer = new LinkedHashSet<>();
-        
-        for(int i = 0; i<list.size(); i++){
-            List<Integer> l = list.get(i);
-            for(int j : l){
-                if(!answer.contains(j)){
-                    answer.add(j);
-                }
-            }
-        }
-        
-        int[] arr = new int[answer.size()];
-        int k = 0;
-        for(int i : answer){
-            arr[k++] = i;
-        }
-        
-        return arr;
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
