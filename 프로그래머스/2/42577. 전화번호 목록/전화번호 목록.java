bb935633dc23b6394fd91ec86435cf8de0aaa281
jcm <galahad000310@gmail.com>
2025-04-01 16:19:35 +0900

    [level 2] Title: 전화번호 목록, Time: 269.43 ms, Memory: 214 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String number : phone_book) {
+            map.put(number, 1);
+        }
+
+        for (String number : phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String prefix = number.substring(0, i);
+                if (map.containsKey(prefix)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
