0de130fd368c7187ab4628115b781700ba2d08dc
jcm <galahad000310@gmail.com>
2025-05-21 13:18:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 16.53 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,37 +1,41 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int p1 = 0, p2 = 0;
-        int sum = sequence[0];
+        int l = 0, r = 0, sum = sequence[0];
         int[] answer = new int[]{-1, -1};
-        
-        while (p1 <= p2 && p1 < sequence.length && p2 < sequence.length) {
-            if (sum < k) {
-                if (p2 + 1 < sequence.length) {
-                    sum += sequence[++p2];
-                } else {
+        while(l<=r){
+            if(sum < k){
+                if(r+1 < sequence.length){
+                    sum += sequence[++r];
+                }else{
                     break;
                 }
-            } else if (sum > k) {
-                sum -= sequence[p1++];
-            } else {
-                if (renew(answer, p1, p2)) {
-                    answer[0] = p1;
-                    answer[1] = p2;
+            }else if(sum > k){
+                sum -= sequence[l++];
+            }else{
+                if(renew(answer, l, r)){
+                    answer[0] = l;
+                    answer[1] = r;
                 }
-                sum -= sequence[p1++];
+                sum -= sequence[l++];
             }
         }
         return answer;
     }
-    
-    public boolean renew(int[] answer, int p1, int p2) {
-        if (answer[0] == -1) return true;
+    private boolean renew(int[] answer, int l, int r){
+        
+        if(answer[0] == -1) return true;
         
         int length1 = answer[1] - answer[0] + 1;
-        int length2 = p2 - p1 + 1;
+        int length2 = r - l + 1;
+        if(length2 < length1){
+            return true;
+        }
         
-        if (length2 < length1) return true;
-        if (length2 == length1 && p1 < answer[0]) return true;
+        if(length2 == length1){
+            if(l < answer[0]){
+                return true;
+            }
+        }
         
         return false;
     }
