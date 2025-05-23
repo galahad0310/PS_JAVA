e8ccdf63638aac5eeef762c76bf93ea20e2ed598
jcm <galahad000310@gmail.com>
2025-05-23 19:03:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,20 +1,20 @@
 class Solution {
-    
     public int solution(int storey) {
         int answer = 0;
         while(storey > 0){
-            int last = storey%10;
+            int k = storey % 10;
             storey /= 10;
-            
-            if(last > 5){
-                answer += (10-last);
+            if(k > 5){
+                answer += (10 - k);
                 storey++;
-            }else if(last < 5){
-                answer += last;
+            }else if(k < 5){
+                answer += k;
             }else{
-                answer += 5;
                 if(storey % 10 >= 5){
+                    answer += (10 - k);
                     storey++;
+                }else{
+                    answer += k;
                 }
             }
         }
