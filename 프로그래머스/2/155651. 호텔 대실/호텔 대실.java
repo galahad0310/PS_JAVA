190d59bcfb256215170344a159badbe2b2d6c7c0
jcm <galahad000310@gmail.com>
2025-05-29 13:31:50 +0900

    [level 2] Title: 호텔 대실, Time: 45.34 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,53 +1,47 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
-        int[][] times = convert_to_minute(book_time);
+        int[][] book_time_minute = new int[book_time.length][2];
+        for(int i = 0; i<book_time_minute.length; i++){
+            book_time_minute[i] = convert_to_minute(book_time[i]);
+        }
         
-        Arrays.sort(times, (a, b) -> {
-            return a[0] > b[0] ? 1 : -1; 
+        Arrays.sort(book_time_minute, (a, b) -> {
+            return a[0] > b[0] ? 1 : -1;
         });
         
-        int room_no = 0;
         Map<Integer, Integer> rooms = new HashMap<>();
-        
-        for(int[] book : times){
-            int start = book[0];
-            int end = book[1];
+        int room_no = 0;
+        for(int[] time : book_time_minute){
+            int s = time[0];
+            int e = time[1];
             
-            boolean checkIn = false;
+            boolean isCheckin = false;
             
-            for(Integer key : rooms.keySet()){
-                if(rooms.get(key) <= start){
-                    rooms.put(key, end + 10);
-                    checkIn = true;
+            for(int key : rooms.keySet()){
+                if(rooms.get(key) <= s){
+                    rooms.put(key, e+10);
+                    isCheckin = true;
                     break;
                 }
             }
             
-            if(!checkIn){
-                rooms.put(room_no++, end + 10);    
+            if(!isCheckin){
+                rooms.put(room_no++, e+10);
             }
         }
         return rooms.size();
     }
     
-    int[][] convert_to_minute(String[][] book_time){
-        int[][] times = new int[book_time.length][2];
+    int[] convert_to_minute(String[] s){
+        int[] arr = new int[2];
         int idx = 0;
-        for(String[] time : book_time){
-            int start = func(time[0]);
-            int end = func(time[1]);
-            times[idx][0] = start;
-            times[idx++][1] = end;
+        
+        for(String str : s){
+            String[] split = str.split(":");
+            arr[idx++] = Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
         }
-        return times;
+        
+        return arr;
     }
-    
-    int func(String s){
-        String[] arr = s.split(":");
-        int a = Integer.parseInt(arr[0]) * 60;
-        int b = Integer.parseInt(arr[1]);
-        return a+b;
-    }
-    
 }
