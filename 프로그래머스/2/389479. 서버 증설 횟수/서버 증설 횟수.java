77c192630a8c42103ea641e74730dca2d9b4b9da
jcm <galahad000310@gmail.com>
2025-05-30 14:55:58 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.87 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,17 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] players, int m, int k) {
-        int answer = 0;
         Queue<Integer> q = new LinkedList<>();
+        int answer = 0;
         
         for(int i = 0; i<players.length; i++){
+            //만료된 서버 반납
             while(!q.isEmpty() && q.peek() <= i){
                 q.poll();
             }
             
-            int needed = players[i] / m;
+            int needed = players[i] / m; //필요한 서버수 계산
+            
+            //현재 서버수가 필요한 서버수보다 적다면 서버 증설
             if(q.size() < needed){
+                //warning!: 다음 for문에서 q를 조작하기 때문에, 
+                //q의 변수값을 for 조건문속에 넣으면 의도와 다르게 돌아감, 
+                //그러므로 미리 값을 밖에서 정의해서 add라는 변수에 담음
                 int add = needed - q.size();
                 for(int j = 0; j<add; j++){
                     q.add(i+k);
