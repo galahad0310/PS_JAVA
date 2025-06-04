ef5f30f61fd620c3f1d1c154af8073e931edc56b
jcm <galahad000310@gmail.com>
2025-06-04 16:24:30 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.67 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    int[] dy = new int[]{-1, 1, 0, 0};
+    int[] dx = new int[]{0, 0, -1, 1};
+    
+    public int solution(String[] board) {
+        char[][] graph = new char[board.length][board[0].length()];
+        int[] start = new int[2];
+        for(int i = 0; i<graph.length; i++){
+            for(int j = 0; j<graph[0].length; j++){
+                graph[i][j] = board[i].charAt(j);
+                if(graph[i][j] == 'R'){
+                    start[0] = i;
+                    start[1] = j;
+                }
+            }
+        }
+        
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[graph.length][graph[0].length];
+        q.add(new int[]{start[0], start[1], 0});
+        visited[start[0]][start[1]] = true;
+        
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            for(int i = 0; i<4; i++){
+                int y = curr[0];
+                int x = curr[1];
+                
+                while(true){
+                    int ny = y + dy[i];
+                    int nx = x + dx[i];
+                    if(!isBoundary(ny, nx, graph) || graph[ny][nx] == 'D') break;
+                    y = ny;
+                    x = nx;
+                }
+                
+                if(graph[y][x] == 'G'){
+                    return curr[2] + 1;
+                }
+                
+                if(!visited[y][x]){
+                    visited[y][x] = true;
+                    q.add(new int[]{y, x, curr[2] + 1});
+                }
+            }
+        }
+        return -1;
+    }
+    
+    boolean isBoundary(int ny, int nx, char[][] graph){
+        return ny > -1 && ny < graph.length && nx > -1 && nx < graph[0].length;
+    }
+}
