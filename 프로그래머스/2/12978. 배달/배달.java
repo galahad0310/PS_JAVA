ad92652c881a6f3135696fef8ee402d5108a7d54
jcm <galahad000310@gmail.com>
2025-06-05 15:04:53 +0900

    [level 2] Title: 배달, Time: 3.44 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,6 +1,6 @@
 import java.util.*;
+
 class Solution {
-    
     class Node implements Comparable<Node>{
         int to;
         int cost;
@@ -8,43 +8,40 @@ class Solution {
             this.to = to;
             this.cost = cost;
         }
-        @Override
         public int compareTo(Node o){
             return this.cost - o.cost;
         }
     }
-    
     public int solution(int N, int[][] road, int K) {
         List<List<Node>> graph = new ArrayList<>();
         for(int i = 0; i<=N; i++) graph.add(new ArrayList<>());
-        
         for(int[] r : road){
             graph.get(r[0]).add(new Node(r[1], r[2]));
             graph.get(r[1]).add(new Node(r[0], r[2]));
         }
-        
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
+        int[] d = new int[N+1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        d[1] = 0;
         
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(1, 0));
         
         while(!pq.isEmpty()){
             Node curr = pq.poll();
-            for(Node next : graph.get(curr.to)){
-                int newDist = dist[curr.to] + next.cost;
-                if(dist[next.to] > newDist){
-                    dist[next.to] = newDist;
-                    pq.offer(new Node(next.to, newDist));
+            for(Node n : graph.get(curr.to)){
+                int newDist = d[curr.to] + n.cost;
+                if(d[n.to] > newDist){
+                    d[n.to] = newDist;
+                    pq.offer(new Node(n.to, newDist));
                 }
             }
         }
         
         int cnt = 0;
-        for(int i = 1; i<=N; i++){
-            if(dist[i] <= K) cnt++;
+        for(int i : d){
+            if(i <= K) cnt++;
         }
+        
         return cnt;
     }
 }
