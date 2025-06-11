6def4f427a516745a5398a8b2c013a4d0823bf00
jcm <galahad000310@gmail.com>
2025-06-11 15:45:36 +0900

    [level 2] Title: 올바른 괄호, Time: 19.41 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Deque<Character> d = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(c == ')'){
+                if(!d.isEmpty()){
+                    if(d.getLast() == '('){
+                        d.removeLast();                        
+                    }
+                }else{
+                    return false;
+                }
+            }else{
+                d.addLast(c);
+            }
+        }
+        return d.isEmpty();
+    }
+}
