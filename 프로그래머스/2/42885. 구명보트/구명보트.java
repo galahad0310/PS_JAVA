647cc75191f1fda0ca455d742cb3bc2ee09612b2
jcm <galahad000310@gmail.com>
2025-06-12 19:01:08 +0900

    [level 2] Title: 구명보트, Time: 11.78 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,16 +4,13 @@ class Solution {
         int cnt = 0;
         int i = 0, j = people.length - 1, sum = 0;
         Arrays.sort(people);
+        //greedy 전략: 가장 무거운 사람은 무조건 태워야 된다 
         while(i<=j){
-            sum = people[i] + people[j];
-            if(sum > limit){
-                j--;
-                cnt++;
-            }else{
+            if(people[i] + people[j] <= limit){
                 i++;
-                j--;
-                cnt++;
             }
+            j--;
+            cnt++;
         }
         return cnt;
     }
