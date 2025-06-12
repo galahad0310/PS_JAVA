98929183ac1979df91bf741cf1d19b01c3efe5ec
jcm <galahad000310@gmail.com>
2025-06-12 14:35:54 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,15 @@
+import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int plus = (brown+4)/2;
-        int mul = brown + yellow;
-        int a = 1, b = 1;
-        
-        for(int i = 1; i<=mul; i++){
-            for(int j = 1; j<=mul; j++){
-                if(i * j == mul && i+j == plus){
-                    return new int[]{Math.max(i, j), Math.min(i,j)};
+        int sum = brown+yellow;
+        for(int i = 1; i<=Math.sqrt(sum); i++){
+            if(sum % i == 0){
+                int j = sum/i;
+                if(i+j == (brown + 4)/2){
+                    return new int[]{Math.max(i, j), Math.min(i, j)};
                 }
             }
         }
-        
         return null;
     }
 }
