c43a765d3e4a31d0feb96b37d2d74ab2a1fd3ce4
jcm <galahad000310@gmail.com>
2025-06-13 14:31:18 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,11 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int cnt = 1;
-        int t1 = (a+1) / 2;
-        int t2 = (b+1) / 2;
-        while(t1!=t2){
+        int cnt = 0;
+        while(a!=b){
             cnt++;
-            t1 = (t1+1) / 2;
-            t2 = (t2+1) / 2;
+            a = (a+1) / 2;
+            b = (b+1) / 2;
         }
         return cnt;
     }
