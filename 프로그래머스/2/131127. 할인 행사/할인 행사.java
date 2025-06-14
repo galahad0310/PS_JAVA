d7451a6559fdb563eeba2a5691d7587e8a13475f
jcm <galahad000310@gmail.com>
2025-06-14 19:07:50 +0900

    [level 2] Title: 할인 행사, Time: 94.15 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,32 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wantMap = new HashMap<>();
-        for(int i = 0; i<want.length; i++){
-            wantMap.put(want[i], number[i]);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i<number.length; i++){
+            map.put(want[i], number[i]);
         }
-        
         int r = 0;
-        
-        for(int i = 0; i<discount.length - 9; i++){
-            Map<String, Integer> map = new HashMap<>();
-
+        for(int i = 0; i<= discount.length - 10; i++){
+            Map<String, Integer> map2 = new HashMap<>();
+            
             for(int j = i; j<i+10; j++){
-                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+                map2.put(discount[j], map2.getOrDefault(discount[j], 0) + 1);
             }
             
-            boolean isPossible = true;
+            boolean b = true;
             
-            for(String key : wantMap.keySet()){
-                if(wantMap.get(key) != map.get(key)){
-                    isPossible = false;
-                    break;
+            for(String key : map.keySet()){
+                if(map2.containsKey(key)){
+                    if(map2.get(key) < map.get(key)){
+                        b = false;
+                    }
+                }else{
+                    b = false;
                 }
             }
             
-            if(isPossible) r++;
+            if(b) r++;
         }
-        
         return r;
     }
 }
