f2b221ca0675f496438029598217123a86d8edd2
jcm <galahad000310@gmail.com>
2025-06-14 15:08:26 +0900

    [level 2] Title: 할인 행사, Time: 94.60 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,27 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        Map<String, Integer> wantMap = new HashMap<String, Integer>();
+        Map<String, Integer> wantMap = new HashMap<>();
         for(int i = 0; i<want.length; i++){
             wantMap.put(want[i], number[i]);
         }
         
-        for(int start = 0; start <= discount.length - 10; start++){
-            Map<String, Integer> cand = new HashMap<String, Integer>();
-            for(int i = start; i < start + 10; i++){
-                cand.put(discount[i], cand.getOrDefault(discount[i], 0)+1);
+        int r = 0;
+        
+        for(int i = 0; i<discount.length - 9; i++){
+            Map<String, Integer> map = new HashMap<>();
+
+            for(int j = i; j<i+10; j++){
+                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
             }
-            if(isValid(wantMap, cand)) answer++;
-        }
-        return answer;
-    }
-    boolean isValid(Map<String, Integer> wantMap, Map<String, Integer> cand){
-        for(String wantKey : wantMap.keySet()){
-            if(!wantMap.get(wantKey).equals(cand.getOrDefault(wantKey, 0))){
-                return false;
+            
+            boolean isPossible = true;
+            
+            for(String key : wantMap.keySet()){
+                if(map.containsKey(key)){
+                    if(map.get(key) < wantMap.get(key)){
+                        isPossible = false;
+                        break;
+                    }
+                }else{
+                    isPossible = false;
+                    break;
+                }
             }
+            
+            if(isPossible) r++;
         }
-        return true;
+        
+        return r;
     }
 }
