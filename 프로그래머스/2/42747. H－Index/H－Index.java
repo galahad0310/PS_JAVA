189a832ad09b870f27c6d58731317a9c928c15cc
jcm <galahad000310@gmail.com>
2025-06-16 11:47:58 +0900

    [level 2] Title: H-Index, Time: 1.39 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,20 +1,11 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int idx = 0;
-        int answer = 0;
-        for(int h = 0; h<citations[citations.length - 1]; h++){
-            while(h > citations[idx]){
-                idx++;
-            }
-            int c1 = citations.length - idx;
-            int c2 = idx+1;
-            
-            if(c1 >= h && c2 <= h){
-                answer = h;
-            }
-        }
-        return answer;
+        for (int i = 0; i < citations.length; i++)
+            if (citations[i] >= citations.length - i)
+                return citations.length - i;
+        return 0;
     }
 }
