7a267c440f815f95cdfe6a1979a8ea51cf48e2f1
jcm <galahad000310@gmail.com>
2025-06-17 15:13:11 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,14 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        for(String[] s : clothes){
-            map.put(s[1], map.getOrDefault(s[1], 0)+1);
+        
+        for(String[] arr : clothes){
+            map.put(arr[1], map.getOrDefault(arr[1], 0)+1);
         }
-        int answer = 1;
-        for(int value : map.values()){
-            answer *= (value+1);
+        
+        int v = 1;
+        
+        for(int i : map.values()){
+            v*=(i+1);
         }
-        return answer-1;
+        
+        return --v;
     }
 }
