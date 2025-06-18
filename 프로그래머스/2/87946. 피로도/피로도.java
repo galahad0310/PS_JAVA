53b7367447d036aa35c3b1c7df504c32d0af49d9
jcm <galahad000310@gmail.com>
2025-06-18 15:10:52 +0900

    [level 2] Title: 피로도, Time: 4.43 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,34 @@
 import java.util.*;
-
 class Solution {
     int max;
     int[][] dungeons;
     boolean[] visited;
-
+    
     public int solution(int k, int[][] dungeons) {
         this.dungeons = dungeons;
-        this.visited = new boolean[dungeons.length];
+        visited = new boolean[dungeons.length];
         dfs(0, k);
+        
         return max;
     }
-
+    
     private void dfs(int depth, int k){
+        if(max == dungeons.length) return;
         for(int i = 0; i<dungeons.length; i++){
             if(!visited[i]){
                 visited[i] = true;
+                
+                //입장 불가
                 if(k < dungeons[i][0]){
                     max = Math.max(max, depth);
                 }else{
-                    dfs(depth+1, k-dungeons[i][1]);
+                    dfs(depth + 1, k - dungeons[i][1]);
                 }
+                
                 visited[i] = false;
             }
         }
-        max = Math.max(depth, max);
-    }
+        
+        max = Math.max(max, depth);
+    }    
 }
