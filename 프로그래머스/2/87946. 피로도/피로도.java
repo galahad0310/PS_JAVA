7f924d141398c787d0060b36dd027cc1e2dd6651
jcm <galahad000310@gmail.com>
2025-06-18 14:58:33 +0900

    [level 2] Title: 피로도, Time: 2.78 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,50 +1,29 @@
 import java.util.*;
+
 class Solution {
-    static int[] selected;
-    static int max = 0;
-    static int[][] d;
-    static boolean[] visited;
-    
+    int max;
+    int[][] dungeons;
+    boolean[] visited;
+
     public int solution(int k, int[][] dungeons) {
-        d = dungeons;
-        int n = dungeons.length;
-        visited = new boolean[n];
-        selected = new int[n];
-    
-        generatePermutations(0, k);
-        
+        this.dungeons = dungeons;
+        this.visited = new boolean[dungeons.length];
+        dfs(0, k);
         return max;
     }
-    
-    public void generatePermutations(int d, int k){
-        if(d == selected.length){
-            simulateRun(selected, k);
-            return;
-        }
-        
-        for(int i = 0; i<selected.length; i++){
+
+    private void dfs(int depth, int k){
+        for(int i = 0; i<dungeons.length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                selected[d] = i;
-                generatePermutations(d+1, k);
+                if(k < dungeons[i][0]){
+                    max = Math.max(max, depth);
+                }else{
+                    dfs(depth+1, k-dungeons[i][1]);
+                }
                 visited[i] = false;
             }
-            
         }
+        max = Math.max(depth, max);
     }
-    
-    public void simulateRun(int[] selected, int k){
-        int total = 0;
-        for(int idx : selected){
-            int[] dungeon = d[idx];
-            if(k >= dungeon[0]){
-                k-=dungeon[1];
-                total++;
-            }else{
-                break;
-            }
-        }
-        max = Math.max(total, max);
-    }
-    
 }
