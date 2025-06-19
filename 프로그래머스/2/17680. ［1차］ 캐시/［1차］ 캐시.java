69e9a277c2fa9693c1e380dd983dc9ed7137270f
jcm <galahad000310@gmail.com>
2025-06-19 11:15:54 +0900

    [level 2] Title: [1차] 캐시, Time: 34.90 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,32 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         if(cacheSize == 0) return cities.length * 5;
-        LRU<String, Boolean> cache = new LRU<String, Boolean>(cacheSize);
         int time = 0;
+        Queue<String> q = new LinkedList<>();
         for(String s : cities){
-            String key = s.toLowerCase();
-            if(cache.containsKey(key)){
+            s = s.toLowerCase();
+            if(q.remove(s)){
                 time+=1;
             }else{
+                if(q.size() >= cacheSize){
+                    q.poll();
+                }
                 time+=5;
             }
-            cache.put(key, true);
+            q.add(s);
         }
         return time;
-    }
-    
-    class LRU<K, V> extends LinkedHashMap<K, V>{
-        private final int capacity;
-        
-        public LRU(int capacity){
-            super(capacity, 0.75f, true);
-            this.capacity = capacity;
-        }
-        
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, V> eldest){
-            return size() > capacity;
-        }
-        
     }
 }
