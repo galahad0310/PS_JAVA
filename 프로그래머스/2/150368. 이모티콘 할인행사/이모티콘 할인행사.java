2e6f76eb77850ac458e18cf4f2fbaae68822d0e6
jcm <galahad000310@gmail.com>
2025-09-03 22:37:16 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 124.24 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,38 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[][] users, int[] emoticons) {
-        List<List<Integer>> permute = permutation(4, emoticons.length);
-        int[][] result = new int[permute.size()][2];
+        List<List<Integer>> list = permute(4, emoticons.length);
+        int[][] notes = new int[list.size()][2];
         
-        for(int i = 0; i<result.length; i++){
-            List<Integer> list = permute.get(i);
+        for(int i = 0; i<notes.length; i++){
+            List<Integer> ratios = list.get(i);
             for(int[] user : users){
-                int want_sale = user[0]; //원하는 할인율
-                int want_limit = user[1]; //이 금액이 넘어가면 이모티콘 플러스 삼
-                double totalPrice = 0; //산 금액 저장
-                
-                for(int j = 0; j<list.size(); j++){
-                    if(list.get(j) >= want_sale){
-                        double x = (1.0 * 100 - list.get(j)) / 100;
-                        totalPrice += x * emoticons[j];
+                int want_ratio = user[0];
+                int limit_price = user[1];
+                double total = 0;
+                for(int j = 0; j<ratios.size(); j++){
+                    if(ratios.get(j) >= want_ratio){
+                        total += ((1.0 * 100 - ratios.get(j)) / 100) * emoticons[j];
                     }
-                    if(totalPrice >= want_limit) break;
+                    if(total >= limit_price) break;
                 }
-                
-                if(totalPrice >= want_limit){
-                    result[i][0]++;
+                if(total >= limit_price){
+                    notes[i][0]++;
                 }else{
-                    result[i][1] += totalPrice;
+                    notes[i][1] += total;
                 }
             }
         }
-        Arrays.sort(result, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o2[0] - o1[0]);
-        return result[0];
+        
+        Arrays.sort(notes, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o2[0] - o1[0]);
+        
+        return notes[0];
     }
     
-    List<List<Integer>> permutation(int n, int r){
+    List<List<Integer>> permute(int n, int r){
         List<List<Integer>> result = new ArrayList<>();
-        permuteHelper(n, r, new ArrayList<Integer>(), result);
+        permuteHelper(n, r, new ArrayList<>(), result);
         return result;
     }
     
