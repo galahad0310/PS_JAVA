384f07433691f28ff97fa32e5c6d1063580b93ac
jcm <galahad000310@gmail.com>
2025-09-12 15:51:08 +0900

    [level 2] Title: 비밀 코드 해독, Time: 149.13 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -5,16 +5,15 @@ class Solution {
         int result = 0;
         
         for(List<Integer> candidate : cases){
+            Set<Integer> hashSet = new HashSet<>(candidate);
             boolean isPossible = true;
             for(int i = 0; i<q.length; i++){
                 int[] input = q[i];
                 int response = ans[i];
                 int cnt = 0;
                 //몇개나 맞는지 세는 과정
-                for(int t : candidate){
-                    for(int j = 0; j<input.length; j++){
-                        if(input[j] == t) cnt++;
-                    }    
+                for(int j = 0; j<input.length; j++){
+                    if(hashSet.contains(input[j])) cnt++;
                 }
                 
                 if(cnt != response){
