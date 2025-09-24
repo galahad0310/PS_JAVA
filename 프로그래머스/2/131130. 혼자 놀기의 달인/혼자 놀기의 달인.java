cce388fbd4903d423702b2ad4354698e48046d3b
jcm <galahad000310@gmail.com>
2025-09-24 11:51:06 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.97 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,24 +1,26 @@
 import java.util.*;
 class Solution {
+    List<Integer> groupSize = new ArrayList<>();
     public int solution(int[] cards) {
         boolean[] visited = new boolean[cards.length];
-        List<Integer> groupSize = new ArrayList<>();
-        
         for(int i = 0; i<cards.length; i++){
             if(!visited[i]){
-                int currentGroupSize = 0;
-                int currentIndex = i;
-                while(!visited[currentIndex]){
-                    visited[currentIndex] = true;
-                    currentIndex = cards[currentIndex] - 1;
-                    currentGroupSize++;
-                }
-                groupSize.add(currentGroupSize);
+                dfs(cards, i, visited, 0);
             }
         }
-        
-        if(groupSize.size() < 2) return 0;
+        if(groupSize.size() < 2){
+            return 0;
+        }
         groupSize.sort(Collections.reverseOrder());
         return groupSize.get(0) * groupSize.get(1);
+    }
+    
+    void dfs(int[] cards, int currentIndex, boolean[] visited, int currentSize){
+        if(visited[currentIndex]){
+            groupSize.add(currentSize);
+            return;
+        }
+        visited[currentIndex] = true;
+        dfs(cards, cards[currentIndex] - 1, visited, currentSize+1);
     }
 }
