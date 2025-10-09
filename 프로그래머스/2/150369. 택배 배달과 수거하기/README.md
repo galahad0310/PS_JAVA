472f77999ca14e2de5e06ec50d7f173e5b2c79c8
jcm <galahad000310@gmail.com>
2025-10-09 12:06:53 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.21 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 92.5 MB, 시간: 13.49 ms
+메모리: 82.4 MB, 시간: 11.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 08일 15:04:49
+2025년 10월 09일 12:06:49
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,19 +1,16 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int deliver = 0, pickup = 0;
-        
+        int d = 0, p = 0;
         for(int i = n-1; i>-1; i--){
-            deliver += deliveries[i];
-            pickup += pickups[i];
-            while(deliver > 0 || pickup > 0){
-                answer += (long)(i + 1) * 2; 
-                deliver -= cap;
-                pickup -= cap;
+            d += deliveries[i];
+            p += pickups[i];
+            while(d > 0 || p > 0){
+                d -= cap;
+                p -= cap;
+                answer += (long)(i+1)*2;
             }
         }
-        
         return answer;
-        
     }
 }
