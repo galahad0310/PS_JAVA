f6f2cddac758381628d475f98ccacd509d5e19e0
jcm <galahad000310@gmail.com>
2025-11-21 22:26:41 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 369 ms, Memory: 144,104 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Solution {
+public class Solution{
+
+    static class Product{
+        int price;
+        int idx;
+        Product(int price, int idx){
+            this.price = price;
+            this.idx = idx;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine().trim()); // 테스트 케이스 개수
-        StringBuilder sb = new StringBuilder();
-
-        for (int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine().trim()); // 날짜 수
-            int[] price = new int[N];
-
+        int t = Integer.parseInt(br.readLine());
+        for(int tc = 0; tc < t; tc++){
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                price[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0; i<n; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            long profit = 0; // 이익은 long!
-            int maxPrice = 0;
+            int max = 0;
+            long profit = 0;
 
-            // 오른쪽(마지막 날)에서 왼쪽(첫날)으로 탐색
-            for (int i = N - 1; i >= 0; i--) {
-                if (price[i] > maxPrice) {
-                    // 앞으로 이 날이 최고가 => 이 날에 판다고 생각
-                    maxPrice = price[i];
-                } else {
-                    // 이 날에는 사고, maxPrice 날에 판다고 가정
-                    profit += (maxPrice - price[i]);
+            for(int i = n-1; i>-1; i--){
+                if(arr[i] > max){
+                    max = arr[i];
+                }else{
+                    profit += (max - arr[i]);
                 }
             }
 
-            sb.append('#').append(tc).append(' ').append(profit).append('\n');
+            System.out.printf("#%d %d\n", tc+1, profit);
         }
-
-        System.out.print(sb.toString());
     }
 }
