01da439cbde72171ed119ca835a98f5a41a2530e
jcm <galahad000310@gmail.com>
2025-11-29 16:22:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,24 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm> //max, min 함수 사용
-#include <sstream> // 문자열파싱을 위해 사용 
-#include <climits> // INT_MAX, INT_MIN 사용
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
 string solution(string s) {
-    int maxVal = INT_MIN;
-    int minVal = INT_MAX;
+    string answer = "";
     
     stringstream ss(s);
-    string temp;
+    int num;
+    vector<int> v; 
     
-    while(ss >> temp){
-        int num = stoi(temp);
-        
-        maxVal = max(maxVal, num);
-        minVal = min(minVal, num);
+    while(ss >> num){
+        v.push_back(num);
     }
     
-    return to_string(minVal) + " " + to_string(maxVal);
+    sort(v.begin(), v.end());
+    
+    return to_string(v.front()) + " " + to_string(v.back());
 }
