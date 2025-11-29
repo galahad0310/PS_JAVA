f9517d1048b0ed5ce121f949791f8731ea05b766
jcm <galahad000310@gmail.com>
2025-11-29 16:04:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.26 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(String s) {
-        String[] arr = s.split(" ");
-        long min = Long.MAX_VALUE;
-        long max = Long.MIN_VALUE;
-        
-        for(String n : arr){
-            min = Math.min(Long.parseLong(n), min);
-            max = Math.max(Long.parseLong(n), max);
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(String n : s.split(" ")){
+            int i = Integer.parseInt(n);
+            max = Math.max(max, i);
+            min = Math.min(min, i);
         }
         
-        return min+" "+max;
+        return min + " " + max;
     }
 }
