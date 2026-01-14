2c9804898272dc8f9c279b4c9f2d9588e692cb4b
jcm <galahad000310@gmail.com>
2026-01-15 06:16:37 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,22 +1,23 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string s) {
-    stringstream ss(s); //c++은 이렇게, 객체를 stack에 저장할 수 있다.
-    int next;
-    ss >> next;
+    stringstream ss(s);
+    int num;
+    ss >> num;
     
-    int min_val = next;
-    int max_val = next;
+    int min_val = num;
+    int max_val = num;
     
-    while(ss >> next){
-        min_val = min(min_val, next);
-        max_val = max(max_val, next);
+    while(ss >> num){
+        min_val = min(min_val, num);
+        max_val = max(max_val, num);
     }
     
     return to_string(min_val) + " " + to_string(max_val);
+    
 }
