96f9e02c132c4f4a24ee7792b2b96b5b96a799d0
jcm <galahad000310@gmail.com>
2026-01-26 04:58:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 88 ms, Memory: 19980 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -13,21 +14,21 @@ int main() {
 	cin >> n >> m;
 
 	unordered_map<string, int> map1;
-	unordered_map<int, string> map2;
+	vector<string> v;
 
 	string input;
 
 	for (int i = 1; i <= n; i++) {
 		cin >> input;
 		map1[input] = i;
-		map2[i] = input;
+		v.push_back(input);
 	}
 
 	string p;
 	for (int i = 0; i < m; i++) {
 		cin >> p;
 		if (isdigit(p[0])) {
-			cout << map2[stoi(p)] << "\n";
+			cout << v[stoi(p) - 1] << "\n";
 		}
 		else {
 			cout << map1[p] << "\n";
